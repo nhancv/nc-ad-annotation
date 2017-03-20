e177a63dbf3c9198034cd2b8358572205f06c21b
Nhan Cao <nhancv92@gmail.com>
2017-03-20 17:27:14 +0700

    fix build tool

--- nannotation/src/main/java/com/nhancv/nannotation/Data.java
+++ nannotation/src/main/java/com/nhancv/nannotation/Data.java
@@ -1,12 +1,14 @@
 package com.nhancv.nannotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  * Created by nhancao on 3/20/17.
  */
-
+@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE) //on class level
 public @interface Data {
 }
